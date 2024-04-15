991b2d6fa12b310b3d0cbc96e053257c12394016
diegojq <dajqprogramming@gmail.com>
2024-04-15 08:33:19 -0500

    the background variable was removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,6 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-            backgroundColor: theme => ({
-        ...theme("colors"),
-        "black":"#141414",
-        "dark-grey":"#1f1f1f",
-        "grey":"#333333",
-        "white":"#ffffff",
-        "neon-green":"#c4f829",
-      }),
-
       colors:{
         "black":"#141414",
         "dark-grey":"#1f1f1f",
